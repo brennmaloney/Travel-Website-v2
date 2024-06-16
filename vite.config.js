307fd7613b0856ce114e64affd2a809ab8d039dc
Brennan Maloney <brennan.maloney02@gmail.com>
2024-06-16 14:41:47 -0400

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	build: {
+        outDir: 'public', // Ensure this is correctly set
+    },
 	plugins: [sveltekit()]
 });
